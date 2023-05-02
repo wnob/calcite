2e8cbdcd2fdfcd0e7b7bdd05a3b5023405f4f3f4
Will Noble <wnoble@google.com>
2023-05-02 16:36:12 -0700

    [CALCITE-5549] Set DATA_TYPE and IS_GENERATEDCOLUMN values for MEASURE metadata (#24)

--- core/src/main/java/org/apache/calcite/jdbc/CalciteMetaImpl.java
+++ core/src/main/java/org/apache/calcite/jdbc/CalciteMetaImpl.java
@@ -484,7 +484,13 @@ public class CalciteMetaImpl extends MetaImpl {
                   : DatabaseMetaData.columnNoNulls,
               precision,
               field.getIndex() + 1,
-              field.getType().isNullable() ? "YES" : "NO");
+              field.getType().isNullable() ? "YES" : "NO",
+              /*isAutoincrement=*/
+              "NO",
+              /*isGeneratedcolumn=*/
+              field.getType().getSqlTypeName() == SqlTypeName.MEASURE
+                  ? "YES"
+                  : "NO");
         });
   }
 
